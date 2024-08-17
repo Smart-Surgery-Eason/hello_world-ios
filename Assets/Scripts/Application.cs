d80ee809627ab1b7b4e949f53a1ce6a9bfc2c17e
Eason <s7887177@gmail.com>
2024-08-17 12:54:50 +0800

    c

--- Assets/Scripts/Application.cs
+++ Assets/Scripts/Application.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -12,12 +13,38 @@ namespace Eason.HelloWorldIos
         [SerializeField] private Button _changeStatusButton;
         [SerializeField] private TextMeshProUGUI _statusText;
 
+        [SerializeField] private TMP_InputField _filePathInputField;
+        [SerializeField] private Button _loadButton;
+        [SerializeField] private TMP_InputField _textEditorInputField;
+        [SerializeField] private Button _saveButton;
+
+
         [SerializeField] private bool _status;
+        [SerializeField] private string _filePathText;
+        [SerializeField] private string _textEditorText;
 
         private void Awake()
         {
             _logHelloWorldButton.onClick.AddListener(LogHelloWorld);
             _changeStatusButton.onClick.AddListener(ChangeStatus);
+
+
+            _textEditorInputField.onValueChanged.AddListener(TextEditorInputFieldValueChanged);
+            _textEditorInputField.text = _textEditorText;
+            _filePathInputField.onValueChanged.AddListener(FilePathInputFieldValueChanged);
+            _filePathInputField.text = _filePathText;
+            _loadButton.onClick.AddListener(Load);
+            _saveButton.onClick.AddListener(Save);
+        }
+
+        private void TextEditorInputFieldValueChanged(string text)
+        {
+            _textEditorText = text;
+        }
+
+        private void FilePathInputFieldValueChanged(string text)
+        {
+            _filePathText = text;
         }
 
         private void ChangeStatus()
@@ -29,6 +56,28 @@ namespace Eason.HelloWorldIos
         private void LogHelloWorld()
         {
             Debug.Log("Hello, World!");
+        }
+
+        private void Load()
+        {
+            var path = Path.Combine(UnityEngine.Application.persistentDataPath, _filePathText);
+            if (!File.Exists(path))
+            {
+                _statusText.text = "File Not Found.";
+                return;
+            }
+            _textEditorText = File.ReadAllText(path);
+            _textEditorInputField.text = _textEditorText;
+        }
+        private void Save()
+        {
+            var path = Path.Combine(UnityEngine.Application.persistentDataPath, _filePathText);
+            var directory = Path.GetDirectoryName(path);
+            if (!Directory.Exists(directory))
+            {
+                _statusText.text = "Folder Not Found.";
+            }
+            File.WriteAllText(path, _textEditorText);
         }
     }
 
